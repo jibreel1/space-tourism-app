5d2be81595a474e608096767a29a5e6dd7fbe6a8
Osunkoya Jibreel <osunkoyajubril6000@gmail.com>
2022-06-14 21:53:25 +0100

    concluded the crew page

--- src/pages/Crew.jsx
+++ src/pages/Crew.jsx
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+import Data from "../constants/data";
 
 const Crew = () => {
-   return <div>Crew</div>;
+   const [crews] = useState(Data.crew);
+   const [value, setvalue] = useState(0);
+
+   const { name, images, role, bio } = crews[value];
+
+   return (
+      <div className="space_crew">
+         <div className="crew">
+            <div className="barlow_text crew_header">
+               <p>
+                  <span>02</span>Meet your Crew
+               </p>
+            </div>
+
+            <div className="crew_content">
+               <div className="crew_img">
+                  <img src={images} alt="" />
+                  <div className="line" />
+               </div>
+               <div className="crew_descr">
+                  <div className="crew_nav">
+                     {crews.map((crew, index) => (
+                        <div
+                           className={` ${index === value && "active"}`}
+                           key={index}
+                           onClick={() => setvalue(index)}
+                        ></div>
+                     ))}
+                  </div>
+                  <div className="crew_details">
+                     <p className="crew_role">{role}</p>
+                     <p className="crew_name">{name}</p>
+                     <p className="crew_bio barlow_text_sub">{bio}</p>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+   );
 };
 
 export default Crew;
